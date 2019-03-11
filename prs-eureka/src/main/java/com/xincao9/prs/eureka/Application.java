64daa0a1115b58d0a92e225bd555f5a929a73fd5
xin.cao <xin.cao@asiainnovations.com>
2019-03-11 19:02:41 +0800

    ok

--- prs-eureka/src/main/java/com/xincao9/prs/eureka/Application.java
+++ prs-eureka/src/main/java/com/xincao9/prs/eureka/Application.java
@@ -1,9 +1,18 @@
 package com.xincao9.prs.eureka;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 /**
  *
  * @author xincao9@gmail.com
  */
+@SpringBootApplication
+@EnableEurekaServer
 public class Application {
 
+    public static void main(String... args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
